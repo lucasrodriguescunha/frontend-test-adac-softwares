58ef519d7f1b335b077eb90261f84f56d4b1d891
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2026-01-29 22:28:07 -0300

    fix: update layout of FeatureHero component for improved styling and spacing

--- src/components/sections/Features/FeatureHero.tsx
+++ src/components/sections/Features/FeatureHero.tsx
@@ -3,12 +3,11 @@ import FeaturesHeroBannerImage from '../../../assets/features.png';
 
 export function FeatureHero() {
   return (
-    <section className="w-full grid grid-cols-2 place-items-center">
+    <section className="w-full grid grid-cols-[auto_auto] justify-center items-center gap-x-4 py-10">
       <img
         src={FeaturesHeroBannerImage}
         alt="Illustration of product features"
       />
-
       <FeatureCards />
     </section>
   );
