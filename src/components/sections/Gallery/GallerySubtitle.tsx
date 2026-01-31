63e6f1e198bfba4e09053403f368d993c1326bec
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2026-01-31 13:37:49 -0300

    feat: add GallerySubtitle component

---
+++ src/components/sections/Gallery/GallerySubtitle.tsx
@@ -0,0 +1,9 @@
+export function GallerySubtitle() {
+  return (
+    <p className='text-xl text-white font-normal'>
+      We focus on ergonomics and meeting you where you work.
+      <br />
+      It's only a keystroke away.
+    </p>
+  );
+}
