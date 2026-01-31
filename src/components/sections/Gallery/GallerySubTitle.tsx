1a0ccc07a8e207b17f1d0726e35026c0fa270621
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2026-01-31 13:36:02 -0300

    fix: remove GallerySubtitle

--- src/components/sections/Gallery/GallerySubTitle.tsx
+++
@@ -1,9 +0,0 @@
-export function GallerySubtitle() {
-  return (
-    <p className='text-xl text-white font-normal'>
-      We focus on ergonomics and meeting you where you work.
-      <br />
-      It's only a keystroke away.
-    </p>
-  );
-}
