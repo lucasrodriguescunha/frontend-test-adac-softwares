60cdad3f57236316877b5711b8285af721322630
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2026-02-01 12:21:44 -0300

    feat: add responsive horizontal margins to content cards

--- src/components/sections/Contents/ContentsCard.tsx
+++ src/components/sections/Contents/ContentsCard.tsx
@@ -10,21 +10,31 @@ export function ContentsCard({
   return (
     <section
       aria-labelledby={`content-title-${id}`}
-      className='flex flex-col items-center h-full w-auto bg-[#FFFFFF] rounded-lg gap-8 p-6'
+      className="
+        flex flex-col items-center
+        h-full w-auto
+        bg-[#FFFFFF]
+        rounded-lg
+        gap-8
+        p-6
+
+        mx-10
+        lg:mx-0
+      "
     >
       <h3
         id={`content-title-${id}`}
-        className='text-xl font-medium text-black whitespace-pre-line pt-4'
+        className="text-xl font-medium text-black whitespace-pre-line pt-4"
       >
         {title}
       </h3>
 
-      <p className='text-base font-normal text-[#5C5C5C] whitespace-pre-line'>
+      <p className="text-base font-normal text-[#5C5C5C] whitespace-pre-line">
         {description}
       </p>
 
-      {cta && <div className='mt-2'>{cta}</div>}
-      <img src={image} alt='' />
+      {cta && <div className="mt-2">{cta}</div>}
+      <img src={image} alt="" />
     </section>
   );
 }
